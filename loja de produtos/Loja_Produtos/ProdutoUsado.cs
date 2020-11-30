f70dde08545708752f5aa7020fc761ab7a109ff5
MathNRT <69465306+MathNRT@users.noreply.github.com>
2020-11-30 18:39:53 -0300

    Add files via upload

---
+++ loja de produtos/Loja_Produtos/ProdutoUsado.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Loja_Produtos
+{
+   
+    //criaçao da classe "produto usado"
+    class ProdutoUsado : Produto
+    {
+
+        DateTime fabricacao;
+
+        public DateTime Fabricacao 
+        {
+            get => fabricacao;
+            set => fabricacao = value;
+        }
+
+        public ProdutoUsado() { }
+
+        public ProdutoUsado(string nome, double preco) : base(nome, preco) { }
+
+        public override string precoTag()
+        {
+            return Nome + " (usado) - R$ " + Preco + "(fabricação: " + fabricacao + ")";
+        }
+
+    }
+}
---
+++ loja de produtos/Loja_Produtos/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+
+namespace Loja_Produtos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int contar;
+            List<Produto> produtos = new List<Produto>();
+
+            //coleta do usuario a informaçao da quantidade de produtos
+            Console.WriteLine("Informe a quantidade de Produtos:");
+            contar = int.Parse(Console.ReadLine());
+
+
+            //inicia as perguntas, que vao obter os dados dos produtos desejados
+            for(int i = 0; i<contar; i++)
+            {
+                Produto produto = new Produto();
+
+                
+                Console.WriteLine("Informe o nome do produto:");
+                produto.Nome = Console.ReadLine();
+
+
+
+                Console.WriteLine("Informe o valor do produto:");
+                produto.Preco = double.Parse(Console.ReadLine());
+
+
+
+                Console.WriteLine("informe se o produto é: Usado(U), Comum(C), Importado(I):");
+                string tipo = Console.ReadLine();
+
+
+
+                switch (tipo)
+                {
+                    case "U":
+                        ProdutoUsado produtoUsado = new ProdutoUsado(produto.Nome, produto.Preco);
+                        Console.WriteLine("Informe a data de fabricação (DD/MM/AAAA): ");
+                        produtoUsado.Fabricacao = DateTime.ParseExact(Console.ReadLine(),"dd/MM/yyyy",CultureInfo.InvariantCulture);
+                        produtos.Add(produtoUsado);
+                        break;
+
+
+                    case "C":
+                        produtos.Add(produto);
+                        break;
+
+
+                    case "I":
+                        ProdutoImportado produtoImportado = new ProdutoImportado(produto.Nome, produto.Preco);
+                        Console.WriteLine("Informe a Taxa: ");
+                        produtoImportado.Taxa = double.Parse(Console.ReadLine());
+                        produtos.Add(produtoImportado);
+                        break;
+                }
+
+            }
+
+
+            foreach(Produto obj in produtos)
+            {
+                Console.WriteLine(obj.precoTag());
+            }
+
+        }
+    }
+}
